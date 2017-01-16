f802d76e342939e814ed505ffc9925b9b51e6c2a
yeahnoob <yeahnoob@buaa.edu.cn>
2017-01-16 23:22:21 +0800

    channels, filter

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,35 @@ use std::sync::mpsc::channel;
 
 fn make_vec(start: i32) -> Vec<i32> {
     let mut v = Vec::new();
-    v.push(start);
-    v.push(start+1);
+    for i in 0..10000 {
+        v.push(start + i);
+    }
     v
 }
 
 // print a vector with Closures
 fn print_vec(v: &Vec<i32>, f: &Fn(i32) -> i32) {
-    for i in v.iter() {
+    // output the number less than 5, in `v: &Vec<i32>`
+    for i in v.into_iter().filter(|x| **x < 5) {
         print!("{} ", f(*i));
     }
     println!();
 }
 
-// channels
-
 fn main() {
+    // channels
     let (tx, rx) = channel();
+
+    // transmit to `channels`
     for i in 0..5 {
         let ttx = tx.clone();
-        let v = make_vec(i);
+        // let v = make_vec(i);
         thread::spawn(move || {
-            ttx.send(v).unwrap();
+            ttx.send(make_vec(i)).unwrap();
         });
     }
 
+    // received from `channels`
     for _ in 0..5 {
         let rv = rx.recv().unwrap();
         print_vec(&rv, &|x| x);
